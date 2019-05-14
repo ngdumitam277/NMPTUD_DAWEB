e1c2aeebc66ed87f802e00756f2b30724518b655
ngdumitam277 <18424058@student.hcmus.edu.vn>
2019-05-14 20:13:41 +0700

    thêm backend:
     + thêm api lấy cả khu vực
     + thêm api sửa 1 khu vực theo mã khu vực
     + thêm api xóa 1 khu vực theo mã khu vực

--- BackEnd/server/app/controllers/khuVuc.controller.js
+++ BackEnd/server/app/controllers/khuVuc.controller.js
@@ -34,3 +34,44 @@ exports.taoKhuVuc = async(req, res) => {
         res.send({message: "Lỗi tạo khu vực"})
     }
 };
+
+// lấy tất cả khu vực
+exports.getAllKhuVuc = async(req, res) => {
+    KhuVuc.find({}, {_id: 0, createdAt: 0, updatedAt: 0, __v: 0})
+    .then((result) => {
+        res.send(result)
+    })
+    .catch((err) => {
+        res.send({message: "Lỗi lấy tất cả các khu vực!"})
+        console.log(err, "getAllKhuVuc")
+    })
+};
+
+// sửa 1 khu vực theo mã khu vực
+exports.updateKhuVuc = async(req, res) => {
+    let maKhuVuc = req.params.maKhuVuc
+    let body = req.body
+
+    KhuVuc.findOneAndUpdate({maKhuVuc: maKhuVuc}, body, {new: true})
+    .then((result) => {
+        res.send({message: "ok"})
+    })
+    .catch((err) => {
+        res.send({message: "Lỗi sửa khu vực theo key!"})
+        console.log(err, "updateKhuVuc")
+    })
+};
+
+// xoá 1 khu vực theo mã khu vực
+exports.deleteKhuVuc = async(req, res) => {
+    let maKhuVuc = req.params.maKhuVuc
+
+    KhuVuc.findOneAndRemove({maKhuVuc: maKhuVuc}, {rawResult: true})
+    .then((result) => {
+        res.send({message: "ok"})
+    })
+    .catch((err) => {
+        res.send({message: "Lỗi xoá khu vực theo key!"})
+        console.log(err, "deleteKhuVuc")
+    })
+};
--- BackEnd/server/app/routes/khuVuc.routes.js
+++ BackEnd/server/app/routes/khuVuc.routes.js
@@ -3,4 +3,13 @@ module.exports = (app) => {
 
     // tạo khu vực
     app.post('/web/create/khuvuc', KhuVuc.taoKhuVuc);
+
+    // lấy tất cả khu vực
+    app.get('/web/khuvuc', KhuVuc.getAllKhuVuc);
+
+    // sửa 1 khu vực theo mã khu vực
+    app.put('/web/khuvuc/:maKhuVuc', KhuVuc.updateKhuVuc);
+
+    // xoá 1 khu vực theo mã khu vực
+    app.delete('/web/khuvuc/:maKhuVuc', KhuVuc.deleteKhuVuc);
 }
